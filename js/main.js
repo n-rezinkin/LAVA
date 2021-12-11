e8c29a6d079796372166fb7973c4b6384297fd59
n-rezinkin <89951085+n-rezinkin@users.noreply.github.com>
2021-12-11 21:09:11 +0300

    lava1112.21-09
    lava

--- js/main.js
+++ js/main.js
@@ -10,10 +10,12 @@ function open() {
    document.getElementById('burger').classList.toggle('active')
    document.getElementById('mobile-burger').classList.toggle('active')
    document.getElementById('mobile-menu').classList.toggle('active');
+   document.querySelector('body').classList.toggle('lock-scroll')
 }
 
 function close() {
    document.getElementById('burger').classList.toggle('active')
    document.getElementById('mobile-burger').classList.toggle('active')
    document.getElementById('mobile-menu').classList.toggle('active');
+   document.querySelector('body').classList.toggle('lock-scroll')
 }
